61fd3af037ef5cd0cb135dd0bc736f65176df551
Alexander Gates <agates100@gmail.com>
2022-08-25 13:22:00 -0700

    updated packages

--- repo/package zip.bat
+++ repo/package zip.bat
@@ -1,4 +1,4 @@
-rm Packages.gz
-rm Packages.bz2
+del Packages.gz
+del Packages.bz2
 F:\Documents\GitHub\alexander.github.io\Files\gzip.exe -c9 Packages > Packages.gz
 F:\Documents\GitHub\alexander.github.io\Files\bzip2.exe -c9 Packages > Packages.bz2
